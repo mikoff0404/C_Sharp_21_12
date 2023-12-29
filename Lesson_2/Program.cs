736561f494191329fad13c30634d5c706305a396
Sokolov Michail <misha_fish@mail.ru>
2023-12-30 03:02:40 +0600

    upgrade

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -2,7 +2,7 @@
 // трехзанчное число и удаляет вторую цифру этого числа
 
 Console.Write("Enter number - ");
-int a = Convert.ToInt32(Console.ReadLine()!);
+int a = int.Parse(Console.ReadLine()!);
 
 int b = a / 100 * 10 + a % 10;
 Console.Write(b);
