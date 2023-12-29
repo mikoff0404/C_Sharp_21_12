a9c1e534cdf2de1874db7f735e76ca8702d57efa
Sokolov Michail <misha_fish@mail.ru>
2023-12-29 23:08:15 +0600

    changes 2  in 1 task of 1 lesson

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.Write("Enter 1st number - ");
+int a1 = Convert.ToInt32(Console.ReadLine()!);
+Console.Write("Enter 2nd number - ");
+int b1 = Convert.ToInt32(Console.ReadLine()!);
+
+
+
+
+if (a1 == b1 * b1)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
