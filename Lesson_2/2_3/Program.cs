6e004f4f415a40e6ce6f575bac0d0bdca19ef2ff
Sokolov Michail <misha_fish@mail.ru>
2023-12-30 22:38:51 +0600

    lesson2_task3

--- Lesson_2/2_3/Program.cs
+++ Lesson_2/2_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Enter number 1 - ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter number 2 - ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a % b == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет, " + a % b);
+}
