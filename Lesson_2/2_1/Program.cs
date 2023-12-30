c165f49e8a33490121707ff2800f48d741af4964
Sokolov Michail <misha_fish@mail.ru>
2023-12-30 22:27:05 +0600

    lesson2_task1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿//напишите программу, которая принимает на вход
+// трехзанчное число и удаляет вторую цифру этого числа
+
+Console.Write("Enter number - ");
+int a = int.Parse(Console.ReadLine()!);
+
+int b = a / 100 * 10 + a % 10;
+Console.Write(b);
