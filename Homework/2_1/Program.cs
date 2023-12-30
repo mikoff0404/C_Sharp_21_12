121677a57c2ae8b417da96dcc9d9684f64d68e21
Sokolov Michail <misha_fish@mail.ru>
2023-12-30 23:59:26 +0600

    homework2_task1

---
+++ Homework/2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 1: Напишите программу, которая принимает
+//на вход число и проверяет, кратно ли оно
+//одновременно 7 и 23
+
+Console.WriteLine("Enter number - ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
