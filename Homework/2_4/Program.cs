556a3425e93a4b792025f885b4aac15004f08095
Sokolov Michail <misha_fish@mail.ru>
2023-12-31 00:54:59 +0600

    homework2_task4

---
+++ Homework/2_4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Напишите программу, которая на вход
+//принимает натуральное число N, а на выходе
+//показывает его цифры через запятую.
+
+Console.WriteLine("Введите натуральное число - ");
+int a = int.Parse(Console.ReadLine()!);
+string word = "";
+
+while (a != 0)
+{
+    if (word != "")
+    {
+        word = "," + word;
+    }
+word = a % 10 + word;
+a = a / 10;
+}
+Console.WriteLine(word);
