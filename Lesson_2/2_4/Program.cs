0601400d395c8b20a95840fa1c12dbf334507eb7
Sokolov Michail <misha_fish@mail.ru>
2023-12-30 22:57:31 +0600

    lesson2_task4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Enter number - ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(a / 100 % 10);
+}
