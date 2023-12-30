7195193425dc4ba117b4a4d29d7dcf8898f103d6
Sokolov Michail <misha_fish@mail.ru>
2023-12-31 00:30:29 +0600

    homework2_task3

---
+++ Homework/2_3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает
+//на вход целое число из отрезка [10, 99] и показывает
+//наибольшую цифру числа.
+
+Console.WriteLine("Введите двухзначное число - ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a < 10 && a > 99)
+{
+    Console.WriteLine("Введено не двухзначное");
+}
+else
+{
+    int a1 = a / 10;
+    int a2 = a % 10;
+    if (a1 > a2)
+    {
+        Console.WriteLine(a1);
+    }
+    else
+    {
+        Console.WriteLine(a2);
+    }
+}
+
