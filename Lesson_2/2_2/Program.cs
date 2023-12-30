1a057836817b7797393c8bfc10c2d33bad3e3c63
Sokolov Michail <misha_fish@mail.ru>
2023-12-30 21:57:44 +0600

    lesson_2_task_2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter number - ");
+int a = int.Parse(Console.ReadLine()!);
+
+int num1 = (a / 10) % 10;
+int num2 = a % 10;
+double result = Math.Pow(num1, num2);
+
+Console.WriteLine(result);
+
+
+
+
+
+
