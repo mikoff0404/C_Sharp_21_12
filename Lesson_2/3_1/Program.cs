b530ff82411d1550268c4a8430045633fc62be67
Sokolov Michail <misha_fish@mail.ru>
2024-01-13 02:22:51 +0600

    3семинар1 задача

---
+++ Lesson_2/3_1/Program.cs
@@ -0,0 +1,35 @@
+﻿//напишите программу , которая
+//определяет присутствует ли данное  число в массиве
+
+
+int [] array = {1, 3, 4, 19, 3};
+Console.WriteLine("Enter number - ");
+int a = int.Parse(Console.ReadLine()!);
+int flag = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == a)
+    {
+        flag = 1;
+    }
+}
+if (flag > 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+               
+    
+
+
+
+
+
+
+
+    
+
