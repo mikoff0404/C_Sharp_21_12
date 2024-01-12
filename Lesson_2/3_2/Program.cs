18ef2d8cb17bb097fc25256e8cbe6742701cce9d
Sokolov Michail <misha_fish@mail.ru>
2024-01-13 02:47:01 +0600

    решили вторую задачу

---
+++ Lesson_2/3_2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int[] array = { -10, 5, -3, -5, 1, 6, -7, 4, 9, -2 };
+
+// Инвертировать каждый элемент массива
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = -1 * array[i];
+    Console.Write(array[i] + " ");
+
+}
+
+
+
+
+
