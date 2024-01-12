d39b73a53c5e66d15ee4f31aaeba05a74c1775a8
Sokolov Michail <misha_fish@mail.ru>
2024-01-12 22:14:52 +0600

    Решили 3 задачу

---
+++ Lesson_2/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int n = 5;
+int[] array = { 31, 4, 3, 7, 9 };
+int i = 0;
+int max_weight = array[0];
+
+while (i < n)
+{
+    if (array[i] > max_weight)
+    {
+        max_weight = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine($"Max weight = {max_weight}");
