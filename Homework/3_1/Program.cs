c472d31c7cc4870d5b065232c5078c4be86a52b7
Sokolov Michail <misha_fish@mail.ru>
2024-01-14 01:23:54 +0600

    HomeWork3_Task1

---
+++ Homework/3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int[] array = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3};
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 10 && array[i] <=90)
+    {
+        count++;
+    }
+    
+}
+Console.Write(count);
