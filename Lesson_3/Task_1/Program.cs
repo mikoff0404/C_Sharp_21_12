52e02c07af249886901187042eca2c041de870ed
Sokolov Michail <misha_fish@mail.ru>
2024-01-18 22:29:23 +0600

    Lesson3 task2

--- Lesson_3/Task_1/Program.cs
+++ Lesson_3/Task_1/Program.cs
@@ -1,12 +1,25 @@
 ﻿
 
-double CalculateFormula(int a, int b, int c, int d)
-{
-    double numerat = a * b;
-    int denomerat = c + d;
-    double result = numerat / denomerat;
-    return result;
-}
+// double CalculateFormula(int a, int b, int c, int d)
+// {
+//     double numerat = a * b;
+//     int denomerat = c + d;
+//     double result = numerat / denomerat;
+//     return result;
+// }
 
-double result = CalculateFormula(6, 1, 3, 3);
-Console.Write(result);
+// double result = CalculateFormula(6, 1, 3, 3);
+// Console.Write(result);
+
+void Square(int n)
+{
+    int i = 1;
+    while(i <= n)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+Square(5);
+Square(10);
+Square(15);
