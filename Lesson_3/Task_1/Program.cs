77f9effa471c5f339482389a02b7e880b0fd6486
Sokolov Michail <misha_fish@mail.ru>
2024-01-18 21:50:01 +0600

    lesson 3 task 1

---
+++ Lesson_3/Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numerat = a * b;
+    int denomerat = c + d;
+    double result = numerat / denomerat;
+    return result;
+}
+
+double result = CalculateFormula(6, 1, 3, 3);
+Console.Write(result);
