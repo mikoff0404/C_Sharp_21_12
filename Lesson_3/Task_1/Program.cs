db547c1c580422dde4461051300fee9d3fffedfa
Sokolov Michail <misha_fish@mail.ru>
2024-01-21 23:16:45 +0600

    Черновик1

--- Lesson_3/Task_1/Program.cs
+++ Lesson_3/Task_1/Program.cs
@@ -11,15 +11,64 @@
 // double result = CalculateFormula(6, 1, 3, 3);
 // Console.Write(result);
 
-void Square(int n)
+
+
+// void Square(int n)
+// {
+//     int i = 1;
+//     while(i <= n)
+//     {
+//         Console.Write($"{i*i} ");
+//         i++;
+//     }
+// }
+// Square(5);
+// Square(10);
+// Square(15); 
+
+
+// void ZeroEvenElements(int[] arr)
+// {
+//     for(int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] % 2 == 0)
+//         {
+//             arr[i] = 0;
+//         }
+
+//     }
+// }
+
+// int square(int value)
+// {
+//     int result = value * value;
+//     return result;
+// }
+// void square1(int value1)
+// {
+//     int result1 = value1 + value1;
+//     Console.Write(result1);
+// }
+// square1 (4);
+
+// int number = square (5);
+// Console.WriteLine($" {number}");
+
+double calculate(int a, int b, int c)
 {
-    int i = 1;
-    while(i <= n)
-    {
-        Console.Write($"{i*i} ");
-        i++;
-    }
+    double num1 = a * b;
+    double num2 = num1 / c;
+    return num2;
 }
-Square(5);
-Square(10);
-Square(15);
+
+double num3 = calculate(2, 5, 5);
+Console.Write(num3);
+
+
+
+
+
+
+
+
+
