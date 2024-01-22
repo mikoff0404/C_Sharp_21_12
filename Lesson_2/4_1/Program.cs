d04de3495e77e5a6a92e54d0707b903a163d7ffb
Sokolov Michail <misha_fish@mail.ru>
2024-01-23 01:18:47 +0600

    в процессе решения

---
+++ Lesson_2/4_1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+Random rnd = new Random();
+void ArrGenerate(size);
+{
+    int[] array = new int[size];    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(1, 101);
+    }
+}
+
+int size = 100;
+ArrGenerate(size);
