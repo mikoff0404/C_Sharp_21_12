65417c2e6ab0a8a88b12996243c7ecbd33f8e59c
Sokolov Michail <misha_fish@mail.ru>
2024-01-23 01:45:26 +0600

    промежуточное сохранение

--- Lesson_2/4_1/Program.cs
+++ Lesson_2/4_1/Program.cs
@@ -2,14 +2,24 @@
 
 
 Random rnd = new Random();
-void ArrGenerate(size);
+void ArrGenerate(int size);
 {
     int[] array = new int[size];    
     for (int i = 0; i < size; i++)
     {
-        array[i] = rnd.Next(1, 101);
+        array[i] = rnd.Next(1, 100);
     }
 }
 
-int size = 100;
-ArrGenerate(size);
+void PrintArr(int[] array1)
+{
+    foreach (int e in array1)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+
+ArrGenerate(100);
+PrintArr(array);
+
