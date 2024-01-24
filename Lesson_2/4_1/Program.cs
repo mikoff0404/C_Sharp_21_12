02df2c6ab371a92e19de024447c5491b114a7aa5
Sokolov Michail <misha_fish@mail.ru>
2024-01-24 21:19:18 +0600

    1 задача на функции

--- Lesson_2/4_1/Program.cs
+++ Lesson_2/4_1/Program.cs
@@ -1,14 +1,19 @@
 ﻿
+int[] array = ArrGenerate(10);
 
+int result = CountSimpleNumber(array);
+PrintArr(array);
+ Console.WriteLine($"\nNumber of prime numbers: {result}");
 
-Random rnd = new Random();
-void ArrGenerate(int size);
+int[] ArrGenerate(int size)
 {
+    Random rnd = new Random();
     int[] array = new int[size];    
     for (int i = 0; i < size; i++)
     {
-        array[i] = rnd.Next(1, 100);
+        array[i] = rnd.Next(1, 10);
     }
+    return array;
 }
 
 void PrintArr(int[] array1)
@@ -19,7 +24,36 @@ void PrintArr(int[] array1)
     }
 }
 
+int CountSimpleNumber (int [] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(FindSimpleNum(arr[i]))
+        {
+            count++;
+        }
+     
+    }
+    return count;
+}
 
-ArrGenerate(100);
-PrintArr(array);
+bool FindSimpleNum (int N)
+{
+    if(N < 2)
+        return false;
+    for (int i = 2; i < N; i++)
+    {
+        if (N % i == 0)
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}
+
+
+
+
 
