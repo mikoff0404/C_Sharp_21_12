c65ebb05dd0d8f96d07a9e056e37903149fb4204
Sokolov Michail <misha_fish@mail.ru>
2024-01-25 09:17:45 +0600

    HomeWork_task_3

---
+++ Homework/4_3/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Введите размер массива - "); // вводим размер массива
+int size = int.Parse(Console.ReadLine()!); // 
+
+int[] array1 = GenerateArray(size); // обращаемся к функции генерации массива
+//получаем сгенерированный массив array1
+
+int[] GenerateArray(int size1) // функция генерации массива
+{
+    int[] array = new int[size1];
+    Random rnd = new Random();
+    for (int i = 0; i < size1; i++)
+    {
+        array[i] = rnd.Next(1, 10);
+        Console.Write($"{array[i]} ");
+        
+    }
+    Console.WriteLine(); // перевод строки
+    return array; // возвращаем массив после реализованной генерации
+    
+}
+
+int[] ResultArray = ReverseArray(array1); //обращаемся к функции переворачивания массива
+PrintResult(ResultArray); //обращение к функции вывода массива на консоль 
+
+int[] ReverseArray(int[] arr) // функция разворота массива
+{
+    int newLength = arr.Length;
+    int[] ReversivARR = new int[newLength];
+    for (int i = 0; i < newLength; i++)
+    {
+        ReversivARR[i] = arr[newLength - i - 1];
+    }
+    return ReversivARR;
+}
+
+void PrintResult(int[] array2) //функция вывода массива на консоль
+{
+    foreach (int e in array2)
+    {
+        Console.Write($"{e} ");
+    }
+}
