65d2fe55df4a6610eb83bfb932e3f15b9b2fbe33
Sokolov Michail <misha_fish@mail.ru>
2024-01-25 15:11:57 +0600

    HomeW_task1

---
+++ Homework/4_1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// Console.Write("ввдите число");
+
+
+while (true)
+{
+    Console.Write("введите число сумма цифр которого четная или символ / q / - ");
+    string word = Console.ReadLine()!;
+
+    if(word == "q")
+    {
+        Console.Write("stop because /q/");
+        return;
+    }
+    else
+    {
+        int number = int.Parse(word);
+        if (NumbersSum(number) % 2 == 0)
+        {
+            Console.Write("stop because even sum");
+            return;
+        }
+
+    }
+
+}
+
+int NumbersSum(int m)
+{
+    int count = 0;
+    while(m > 0)
+    {
+    count = m % 10 + count;
+    m = m / 10;
+    }
+    return count;
+}
